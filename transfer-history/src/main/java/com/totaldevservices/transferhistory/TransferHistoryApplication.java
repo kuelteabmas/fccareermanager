01b143a958301c53b150165ec343cd376b7e51b2
kuelteabmas <fallmuhamed@gmail.com>
2024-10-18 18:53:11 -0400

    Added tds-commons module for commons classes
    - Updated transferhistory/build.gradle
    - Updated transferhistory/banner.txt

--- transfer-history/src/main/java/com/totaldevservices/transferhistory/TransferHistoryApplication.java
+++ transfer-history/src/main/java/com/totaldevservices/transferhistory/TransferHistoryApplication.java
@@ -3,9 +3,12 @@ package com.totaldevservices.transferhistory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static com.totaldevservices.TDSCommons.createDbIfNotExists;
+
 @SpringBootApplication
 public class TransferHistoryApplication {
     public static void main(String[] args) {
+        createDbIfNotExists("transferhistorydb", "5892");
         SpringApplication.run(TransferHistoryApplication.class, args);
     }
 }
